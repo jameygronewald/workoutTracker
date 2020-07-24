9b51face1fbbd48911ddbcc425aa450ab9d5b6ea
Jamey Gronewald <jrgronewald@gmail.com>
2020-07-23 20:28:45 -0700

    created server.js and wrote basic express server and connected to mongo database; confirmed app is running on localhost

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/userdb', { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`App running at http://localhost:${PORT}`)
+});
